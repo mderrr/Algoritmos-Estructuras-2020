6a42677ebf308e8dd33176d4cb0704414ad8a9d2
shernandezz <santihernandezzapata@gmail.com>
2020-08-11 13:08:34 -0500

    Quiz 1

---
+++ Examenes/Quiz 1/Comparacion/comparacion.py
@@ -0,0 +1,42 @@
+import time 
+
+def caso1(n):
+
+    if n < 2:
+        return n
+    else:
+        return caso1(n-1) + caso1(n-2)
+    
+def caso2(n):
+  if n < 2:
+        return n
+  else:
+    a=0
+    b=1
+    for i in range(2,n+1):
+      c=a+b
+      a=b
+      b=c
+    return(c)
+
+# Por alguna razon en VS code no funciona pero en repl.it si
+def test(number, cases):
+    elapsed_times = []
+
+    for case in cases:
+
+        start = time.time()
+        print("str: " + str(start))
+        case(number)
+        end = time.time()
+        print("end: " + str(end))
+        print(end -start)
+        elapsed_times.append(end -start)
+        
+    print(elapsed_times)
+
+    for i in range(len(elapsed_times)):
+        if elapsed_times[i] == min(elapsed_times):
+            print("El caso {} es el más eficiente".format(i+1))
+
+test(12, [caso1, caso2])
